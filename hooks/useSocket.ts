082ce9db35d28b53a9ae0e53b74c18a79bc475ed
Idoga Emmanuel <siremms300@gmail.com>
2026-05-18 04:54:40 -0700

    fixed socket

--- hooks/useSocket.ts
+++ hooks/useSocket.ts
@@ -1,7 +1,8 @@
 'use client'
 
 import { useEffect, useRef, useCallback } from 'react'
- import io, { Socket } from 'socket.io-client'
+import io from 'socket.io-client'
+import type { Socket } from 'socket.io-client'
 
 const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:5000'
 
