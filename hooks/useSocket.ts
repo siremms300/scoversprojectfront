0ea82f404acc5da0c9494b7c2c2b6c5403dc9ca6
Idoga Emmanuel <siremms300@gmail.com>
2026-05-18 04:34:39 -0700

    used claude to fix the issue

--- hooks/useSocket.ts
+++ hooks/useSocket.ts
@@ -1,7 +1,7 @@
 'use client'
 
 import { useEffect, useRef, useCallback } from 'react'
-import { io, Socket } from 'socket.io-client'
+ import io, { Socket } from 'socket.io-client'
 
 const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:5000'
 
